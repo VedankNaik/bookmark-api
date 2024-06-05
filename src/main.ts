c4ddd92f14db3d347b5a46deb81847dd1a3200e8
Vedank Naik <vedankn01@gmail.com>
2024-06-05 22:37:02 +0530

    Add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
@@ -9,6 +10,14 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
+  const config = new DocumentBuilder()
+    .setTitle('Bookmark nestjs')
+    .setDescription('The bookmark API description')
+    .setVersion('1.0')
+    .addTag('bookmarks')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
